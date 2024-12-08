c6c34c14a0ad4bfcf1d4f24ba76cedae024a987a
Fiorella Queirolo <FiorellaLucia.Queirolo@autonoma.cat>
2024-12-08 13:15:36 +0100

    corregidos tests, all passed

--- src/test/java/features/addProductToCart.feature
+++ src/test/java/features/addProductToCart.feature
@@ -6,4 +6,4 @@ Scenario: Add one product to cart
 	When the user clicks accept all cookies
 	Then the cart widget shows zero products
 	When the user clicks add to cart the book KIDNAPPERS
-	Then the cart widget shows one product 
+	Then the cart widget shows one product
--- src/test/java/steps/AddProductToCartSteps.java
+++ src/test/java/steps/AddProductToCartSteps.java
@@ -16,34 +16,34 @@ public class AddProductToCartSteps {
 	
     private WebDriver driver;
 
-    @Given("The user opens the homepage")
+    @Given("the user opens the homepage")
     public void theUserOpensTheHomepage() {
     	System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
     	driver = new FirefoxDriver();
     	driver.navigate().to("https://www.normacomics.com");
     }
     
-    @Given("The cookie banner is displayed")
+    @Given("the cookie banner is displayed")
     public void theCookieBannerIsDisplayed() {
     	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     	WebElement cookieDialog = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("CybotCookiebotDialog")));
     	Assert.assertTrue(cookieDialog.isDisplayed());
     }
     
-    @When("The user clicks accept all cookies")
+    @When("the user clicks accept all cookies")
     public void theUserClicksAcceptAllCookies() {
         WebElement acceptButton = driver.findElement(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")); 
         acceptButton.click();
     }
     
-    @Then("The cart widget shows zero products")
+    @Then("the cart widget shows zero products")
     public void theCartWidgetShowsZeroProducts() {
         WebElement cartWidget = driver.findElement(By.cssSelector(".counter-number")); 
         String count = cartWidget.getText().trim();
         Assert.assertEquals("0", count);
     }
     
-    @When("The user clicks add to cart the book KIDNAPPERS")
+    @When("the user clicks add to cart the book KIDNAPPERS")
     public void theUserClicksAddToCartTheBookKIDNAPPERS() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement bookKIDNAPPERS = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.product-item-details")));
@@ -53,7 +53,7 @@ public class AddProductToCartSteps {
         addToCartButton.click();
     }
     
-    @Then("The cart widget shows one product")
+    @Then("the cart widget shows one product")
     public void theCartWidgetShowsOneProduct() {
         WebElement cartWidget = driver.findElement(By.cssSelector(".counter-number"));
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
