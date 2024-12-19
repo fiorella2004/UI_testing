04dfdb7c1916057c6a6bf8a38969d7eb34f4b512
Fiorella Queirolo <FiorellaLucia.Queirolo@autonoma.cat>
2024-12-19 18:57:02 +0100

    feature login terminado, all tests passed

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -1,8 +1,12 @@
 Feature: Login
 
-Scenario: Accept all cookies
-    Given 
-    And 
-    When 
-    Then 
-    And 
+Scenario: Login with an existing account
+    Given the user opens the index page
+    And the cookie banner is displayed
+    When the user clicks accept all cookies
+    And the user clicks the user button
+    Then the user button is displayed
+    When the user writes their email
+    And the user writes their password
+    And the user clicks the login button
+    Then the user is logged
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,5 +1,61 @@
 package steps;
 
-public class LoginSteps {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import utils.WebDriverManager;
+
+public class LoginSteps {
+	
+	protected WebDriver driver = WebDriverManager.getDriver();
+	
+	@And("the user clicks the user button")
+	public void theUserClicksTheUserButton() {
+        WebElement userButton = driver.findElement(By.id("account-dropdown-sinapsis"));
+        userButton.click();
+	}
+	
+	@Then("the user button is displayed")
+	public void theUserButtonIsDisplayed() {
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    	WebElement userButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-form")));
+    	Assert.assertTrue(userButton.isDisplayed());
+	}
+	
+	@When("the user writes their email")
+	public void theUserWritesTheirEmail() {
+		WebElement email= driver.findElement(By.id("email")); 
+		email.clear();
+		email.sendKeys("1666868@uab.cat");
+	}
+	
+	@And("the user writes their password")
+	public void theUserWritesTheirPassword() {
+		WebElement pass = driver.findElement(By.id("pass")); 
+		pass.clear();
+		pass.sendKeys("pruebaTest24");
+	}
+	
+	@And("the user clicks the login button")
+	public void theUserClicksTheLoginButton() {
+		WebElement loginButton = driver.findElement(By.id("send2")); 
+		loginButton.click();
+	}
+	
+	@Then("the user is logged")
+	public void theUserButtonIsLogged() {
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement page = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1.page-title")));
+        String title = page.getText();
+        Assert.assertTrue(title.contains("Mi Área FANZ"));
+	}
 }
