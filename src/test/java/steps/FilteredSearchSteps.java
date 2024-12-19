7fd4d3c8a10491b427cc71329580cf3737f95816
NIU1392328 <meritxell.argente@autonoma.cat>
2024-12-19 20:40:49 +0100

    Test filteredSearch passed

--- src/test/java/steps/FilteredSearchSteps.java
+++ src/test/java/steps/FilteredSearchSteps.java
@@ -1,42 +1,21 @@
 package steps;
 
 import static org.testng.Assert.assertNotNull;
-
 import java.time.Duration;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import io.cucumber.java.After;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import utils.WebDriverManager;
 
 public class FilteredSearchSteps {
 
-	private WebDriver driver;
-	
-	@Given("the user is in the index page")
-    public void theUserIsInTheIndexPage() {
-        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
-        driver = new FirefoxDriver();
-        driver.navigate().to("https://www.normacomics.com/");
-    }
-    
-    @When("The user clicks accept all cookies")
-    public void theUserClicksAcceptAllCookies() {
-    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(600));  
-        WebElement acceptButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")));
-        acceptButton.click();
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("CybotCookiebotDialogBodyUnderlay")));
-    }
+	protected WebDriver driver = WebDriverManager.getDriver();
     
     @And("the user clicks on the Menu button")
     public void theUserClicksOnTheMenuButton() {
@@ -52,10 +31,9 @@ public class FilteredSearchSteps {
         comicsButton.click();
     }
     
-    
     @Then("the user should see a message Norma Comics La Mayor Tienda De Comics De Europa")
     public void theUserShouldSeeAMessageNormaComicsLaMayorTiendaDeComicsDeEuropa() {
-    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));  
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));  
     	WebElement messageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.base[data-ui-id='page-title-wrapper']")));
         String messageText = messageElement.getText();
         Assert.assertTrue(messageText.contains("Norma Cómics: La Mayor Tienda De Cómics De Europa"));
@@ -90,18 +68,4 @@ public class FilteredSearchSteps {
     	WebElement resultsContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.product-image-photo[alt='PORTAMANGA SUSHI']")));
         assertNotNull(resultsContainer);
     }
-    
-    @After
-    public void closeBrowser() {
-        if (driver != null) {
-            try {
-                Thread.sleep(1000);  
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            driver.quit();
-        }
-    }
-    
-
 }
